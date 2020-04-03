176269fa77f90143afa5812c150ab3acf0e5871f
xiaodi48700 <1585737508@qq.com>
2020-04-03 15:14:09 +0800

    Saving before ejecting

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
+class Comp extends Component{
+ 
+  render() {
+    return (
+      <button className="comp" onClick={this.handleClick}>
+      </button>
+    )
+  }
+}
+function add(ax){
+  alert(ax);
+}
+
+class Acs extends Component {
+  constructor(props) {
+    super(props);
+   console.log(this.props);
+  }
+  
+ render(){
+   return(
+   <div>
+    
+    
+   </div>
+    
+   )
+ }
+}
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      count:1
+     };
+  }
+  
+  handleClick=()=>{
+    this.setState((prevState,prevProps)=>({
+      count:prevState.count+1
+    }),
+    ()=>{}
+    )
+  }
+
+  componentWillReceiveProps(props){
+    
+  }
+  componentDidUpdate(){
+    console.log('更新完成')
+  }
+  shouldComponentUpdate(nextProps,nextState){
+    
+    if(nextState.count % 2===0)
+    {
+      console.log('是否更新'); 
+      return true;      
+    }
+    else
+    {
+       console.log('无法更新');
+      return false;     
+    }
+       
+   }
+
+  render() {
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{this.state.count}</div>
+      <button onClick={this.handleClick}>+1</button>          
     </div>
-  );
+    );
+  }
 }
 
 export default App;
